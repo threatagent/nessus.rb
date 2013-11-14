a40dd75eacb1a164a638c5f22dce0a9e30a70ee8
Marcus Carey <marcus@threatagent.com>
2013-11-13 20:01:01 -0600

    Add additional scan and report methods

--- lib/nessus/client/report.rb
+++ lib/nessus/client/report.rb
@@ -5,11 +5,90 @@ module Nessus
       # GET /report/list
       #
       # @return [Array<Hash>] an array of report hashes
-      def reports
-        resp = get '/report/list'
-
-        resp['reply']['contents']['reports']['report']
+      def report_list
+        response = get '/report/list'
+        response['reply']['contents']['reports']['report']
       end
+
+      # GET /file/xslt/list
+      #
+      # @return [Array<Hash>] an object containing a list of XSLT transformations
+      def xslt_list
+        response = post '/file/xslt/list'
+        response['reply']['contents']
+      end
+
+      # POST /report/delete
+      #
+      # @param [String] report unique identifier
+      #
+      # @return status OK if successful
+      def report_delete(report)
+        response = post '/report/delete', :report => report
+        response['reply']['contents']
+      end
+
+      # POST /report/hosts
+      #
+      # @param [String] report unique identifier
+      #
+      # @return status OK if successful
+      def report_hosts(report)
+        response = get '/report/hosts', :report => report
+        response['reply']['contents']
+      end
+
+
+      # POST /report/ports
+      #
+      # @param [String] report unique identifier
+      # @param [String] hostname name of host to display open ports for
+      #
+      # @return an object containing a list of open ports on a specified host
+      def report_ports(report, hostname)
+        arguments = {
+                      :report => report,
+                      :hostname => hostname
+                    }
+        response = post '/report/ports', arguments
+        response['reply']['contents']
+      end
+
+      # POST /report/details
+      #
+      # @param [String] report unique identifier
+      # @param [String] hostname to display scan results for
+      # @param [String] port to display scan results for
+      # @param [String] protocol of open port on host to display scan details for
+      #
+      # @return an object containing a details of specified scan
+      def report_details(report, hostname, port, protocol)
+        arguments = {
+                       :report => report,
+                       :hostname => hostname,
+                       :port => port,
+                       :protocol => protocol
+                     }
+        response = post '/report/details', arguments
+        response['reply']['contents']
+      end
+
+      # POST /report/tags
+      #
+      # @param [String] report unique identifier
+      # @param [String] hostname name of host to display open ports for
+      #
+      # @return an object containing a list of tags for the specified host
+      def report_tags(report, hostname)
+        arguments = {
+                      :report => report,
+                      :hostname => hostname
+                    }
+        response = post '/report/tags', arguments
+        response['reply']['contents']
+      end
+
+
     end
   end
 end
--- lib/nessus/client/scan.rb
+++ lib/nessus/client/scan.rb
@@ -7,10 +7,10 @@ module Nessus
       # @param [String] target a string that contains the scan target(s)
       # @param [Fixnum] policy_id a numeric ID that references the policy to use
       # @param [String] scan_name the name to assign to this scan
-      # @param [Fixnum] seq a unique identifer for the specific request
+      # @param [Fixnum] seq a unique identifier for the specific request
       #
       # @return [Hash] the newly created scan object
-      def create_scan(target, policy_id, scan_name, seq = nil)
+      def scan_new(target, policy_id, scan_name, seq = nil)
         payload = {
           :target => target,
           :policy_id => policy_id,
@@ -18,22 +18,51 @@ module Nessus
           :json => 1
         }
         payload[:seq] = seq if seq
-        resp = post '/scan/new', payload
+        response = post '/scan/new', payload
 
-        if resp['reply']['status'].eql? 'ERROR'
-          raise Nessus::UnknownError, resp['reply']['contents']
+        if response['reply']['status'].eql? 'ERROR'
+          raise Nessus::UnknownError, response['reply']['contents']
         end
 
-        resp['reply']['contents'] # ['scan']
+        response['reply']['contents'] # ['scan']
       end
 
       # GET /scan/list
       #
       # @return [Array<Hash>] an array of scan hashes
-      def scans
-        resp = get '/scan/list'
+      def scan_list
+        response = get '/scan/list'
+        response['reply']['contents']
+      end
 
-        resp['reply']['contents']
+      # POST /scan/stop
+      #
+      # @param [String] scan_uuid unique identifier for the scan
+      #
+      # @return status OK if successful
+      def scan_stop(scan_uuid)
+        response = post '/scan/stop', :scan_uuid => scan_uuid
+        response['reply']['contents']
+      end
+
+      # POST /scan/pause
+      #
+      # @param [String] scan_uuid unique identifier for the scan
+      #
+      # @return status OK if successful
+      def scan_pause(scan_uuid)
+        response = post '/scan/pause', :scan_uuid => scan_uuid
+        response['reply']['contents']
+      end
+
+      # POST /scan/resume
+      #
+      # @param [String] scan_uuid unique identifier for the scan
+      #
+      # @return status OK if successful
+      def scan_resume(scan_uuid)
+        response = post '/scan/resume', :scan_uuid => scan_uuid
+        response['reply']['contents']
       end
     end
   end
